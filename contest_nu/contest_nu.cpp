31c681e9945d6d381d63795f844bd3548c5e4037
DaRRiK <darrikshyngys@gmail.com>
2025-11-11 18:24:16 +0500

    first commit

---
+++ contest_nu/contest_nu.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int result = 1;
+
+    if (n == 1) {
+        cout << result;
+        return 0;
+    }
+
+    result = n + (n - 2);
+
+    cout << result;
+
+    return 0;
+}
